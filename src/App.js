a65a50655fa4ec3962e84e57a960eeacf8713f4d
vaivodeieva <87878157+deieva@users.noreply.github.com>
2021-08-13 16:16:40 +0300

    update App.js, i react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      
-    </div>
+      <Router >
+          <ScrollToTop/>
+          <Header />
+          <Switch>
+              <Route exact path="/">
+                  <Home />
+              </Route>
+          </Switch>
+          <Footer />
+      </Router>
   );
 }
 
