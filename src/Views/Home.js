4780c1925260ab00fb9a0cd0081834d1d4722dc8
vaivodeieva <87878157+deieva@users.noreply.github.com>
2021-08-13 16:37:52 +0300

    update App.css

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -6,13 +6,11 @@ function Home() {
         <div className="test-container">
                     <div className="text-center header-wrapper">
                         <div className="p-4">
-                            <h1 className="pt-1 mb-5">Some title</h1>
                             <div className="button-wrapper">
                                 <NavLink to="/articles">
-                                    <p className="animated-phrase">Discover!</p>
+                                    <p className="animated-phrase">Join!</p>
                                 </NavLink>
                             </div>
-                            <h2 className="pt-4 mt-1 head-info">Some other title</h2>
                         </div>
                 </div>
             </div>
