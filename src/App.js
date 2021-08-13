608b75c68f32f843947a298d34f4108de3d576f6
vaivodeieva <87878157+deieva@users.noreply.github.com>
2021-08-13 16:28:05 +0300

    add components, view

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './Views/Home';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 
 function App() {
   return (
       <Router >
-          <ScrollToTop/>
           <Header />
           <Switch>
               <Route exact path="/">
