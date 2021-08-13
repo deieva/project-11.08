47f7d2becfcee0f89fbdb7279f72a9174c4bf0d3
vaivodeieva <87878157+deieva@users.noreply.github.com>
2021-08-13 16:34:34 +0300

    update App.css

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -3,22 +3,18 @@ import { NavLink } from "react-router-dom";
 function Home() {
 
     return (
-        <div>
-            <div className="row py-5 my-5">
-                <div className="col-12 col-md-6 offset-md-3">
+        <div className="test-container">
                     <div className="text-center header-wrapper">
-                        <div className="header-background p-4">
-                            <h1 className="pt-1 mb-5 main-header head-info">Some title</h1>
+                        <div className="p-4">
+                            <h1 className="pt-1 mb-5">Some title</h1>
                             <div className="button-wrapper">
                                 <NavLink to="/articles">
-                                    <p className="animated-word">Discover!</p>
+                                    <p className="animated-phrase">Discover!</p>
                                 </NavLink>
                             </div>
                             <h2 className="pt-4 mt-1 head-info">Some other title</h2>
                         </div>
-                    </div>
                 </div>
-              </div>
             </div>
             )
 }
