e6dca1d1bb4f65189dbbcaaa4f1e4cd7c9f8cde7
vaivodeieva <87878157+deieva@users.noreply.github.com>
2021-08-16 22:19:35 +0300

    transparent header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,7 @@ function Header() {
     return (
 
         <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="navbar navbar-expand-lg navbar-light bg-transparent">
   <div className="container-fluid">
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
