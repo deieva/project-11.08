dbde832bf724ce7b2f985f848f3e630554f2ee21
vaivodeieva <87878157+deieva@users.noreply.github.com>
2021-08-16 22:22:43 +0300

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -11,16 +11,16 @@ function Header() {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <a className="nav-link active" aria-current="page" href="#" style={{color: 'white'}}>Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
+          <a className="nav-link" href="#" style={{color: 'white'}}>Link</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
+          <a className="nav-link" href="#" style={{color: 'white'}} >Link</a>
         </li>
         <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a className="nav-link dropdown-toggle" href="#" style={{color: 'white'}} id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Dropdown
           </a>
           <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
