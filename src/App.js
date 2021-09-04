0644b54cdd7fe028f4b7b526b0b88fdcf6e390d2
vaivodeieva <87878157+deieva@users.noreply.github.com>
2021-09-04 23:03:00 +0300

    add page404

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './Views/Home';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+import Page404 from './Views/Page404';
 
 function App() {
   return (
@@ -12,6 +13,11 @@ function App() {
               <Route exact path="/">
                   <Home />
               </Route>
+
+              <Route path="/page404">
+                  <Page404 />
+              </Route>
+
           </Switch>
           <Footer />
       </Router>
