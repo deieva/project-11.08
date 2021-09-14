121ef9dda3254ae028d6ee22df0726518c3f021c
vaivodeieva <87878157+deieva@users.noreply.github.com>
2021-09-14 22:11:34 +0300

    update positioning on home.js

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -3,21 +3,22 @@ import { NavLink } from "react-router-dom";
 function Home() {
 
     return (
-        <div className="test-container md-3 sm-3">
-            <h1>
-                Welcome to the nature 
+        <div className="md-3 sm-3">
+            <h1 className="test-container">
+                Welcome to the nature
             </h1>
-                    <div className="text-center header-wrapper sm-12 md-6 lg-12">
-                        <div className="sm-3 md-3 p-3">
-                            <div className="button-wrapper">
-                                <NavLink to="/" className="sm-3">
-                                    <p className="animated-phrase">Join!</p>
-                                </NavLink>
-                            </div>
-                        </div>
+            <div className="text-center header-wrapper sm-12 md-6 lg-12">
+                <div className="sm-3 md-3 p-3">
+                    <div className="button-wrapper">
+                        <NavLink to="/" className="sm-3 test-container2">
+                            <p className="animated-phrase">Join!</p>
+                        </NavLink>
+                    </div>
                 </div>
             </div>
-            )
+
+        </div>
+    )
 }
 
-            export default Home;
+export default Home;
